bb4246d4a034b1d4b974bb8053f695f104d26da5
vasu gupta <vasugupta153@gmail.com>
2024-05-28 18:03:28 +0530

    modified

--- middleware/apiKeyAuth.js
+++ middleware/apiKeyAuth.js
@@ -2,8 +2,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const apiKeyAuth = (req, res, next) => {
-    console.log(req.headers);
-    const apiKey = req.headers['x-api-key'];
+    // console.log(req.headers);
+    const apiKey = req.header('x-api-key');
     if (apiKey !== process.env.SECRET_API_KEY) {
         console.log("Unauthorized Access!")
       return res.status(401).json({ error: 'Unauthorized' });
